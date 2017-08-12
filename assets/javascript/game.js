5c2436dfd4b0d01dc4af8b504d159bba9d89ecf5
Sam Sadler <samsadler9@sams-air.attlocal.net>
2017-08-11 21:27:07 -0500

    baby steps

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+<script type="text/javascript">
+
+    // Words to guess
+    var word = ["par", "birdie", "eagle", "bogie", "fairway", "teebox", "rough", "bunker", "green", "flag", "fore"]
+
+    // tracks the keys pressed on HTML
+    document.onkeyup = function(event) {
+    	userGuess = event.key;
+    	console.log(userGuess);
+    }
+
+    // Blank spaces that coorelate with the hidden letters
+
+    // Display letters chosen
+
+    // Display guesses remaining
+
+    // Show available letters
+
+    // Create a box to track wins and losses
+
+
+
+
+    </script>
