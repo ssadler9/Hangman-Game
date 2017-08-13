d8cb99f4754f3e39275beac3a491d32a5970a45a
Sam Sadler <samsadler9@sams-air.attlocal.net>
2017-08-13 13:41:46 -0500

    challengeGuess now pushes to an empty array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,18 +8,20 @@ var challenge = "";
 var letterBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
-if (letterBank === "a" || "b" || "c" || "d" || "e" || "f" || "g" || "h" || "i" || "j" || "k" || "l" || "m" || "n" || "o" || "p" || "q" || "r" || "s" || "t" || "u" || "v" || "w" || "x" || "y" || "z") {
-	// blank should reveal text
-	console.log();
-} else {
-	// incorrect answer, number of choices goes down
-	console.log();
-}
+// if (letterBank === "a" || "b" || "c" || "d" || "e" || "f" || "g" || "h" || "i" || "j" || "k" || "l" || "m" || "n" || "o" || "p" || "q" || "r" || "s" || "t" || "u" || "v" || "w" || "x" || "y" || "z") {
+// 	// blank should reveal text
+// 	console.log();
+// } else {
+// 	// incorrect answer, number of choices goes down
+// 	console.log();
+// }
 
 // Randomly chooses words to guess
 var theWord = word[Math.floor(Math.random() * word.length)];
 console.log(theWord);
 
+
+// Counts the length of the string, and displays _ spaces
 var answerSpaces = [];
 for (var i = 0; i < theWord.length; i++) {
 	answerSpaces[i] = "_";
@@ -27,42 +29,50 @@ for (var i = 0; i < theWord.length; i++) {
 console.log(answerSpaces);
 
 // tracks the keys pressed on HTML
-
 document.onkeyup = function(event) {
     var userGuess = event.key;
     console.log(userGuess);
 
+
+// pushes the userGuess into an empty array
 var challengeGuess = [];
 for (var i = 0; i < userGuess.length; i++) {
     	challengeGuess[i];
     	challengeGuess.push(userGuess);
     	console.log(challengeGuess);
-    }    
+   
+ } 
+    // Trying to make the guess match a letter in the generated word
+    	var correctGuess = [];
+    	for (var i = 0; i < theWord.length; i++) {
+    		theWord[i]
+    	if (challengeGuess.indexOf(theWord[i])) {
+	// 	console.log("no");
+	// } else if{
+	// 	console.log("yes");
+	// }
+		challengeGuess.push(correctGuess);
+   		console.log(correctGuess);
+   }
+}
 }
 
 
 
-// var challengeGuess = [];
-// for (var i = 0; i < Things.length; i++) {
-// 	Things[i]
-// }
-// }
+
 
 // Blank spaces that coorelate with the hidden letters
 
-// function challengeGuess ()
+
 // Display letters chosen
 
  
 
 // Display guesses remaining
 
-// var guessesLeft = 9
 
-// document.onkeyup = function(keyPressed) {
-// 	var keyPressed = keyPressed.key
-// 	userGuess = String.fromCharCode(word);
-// }
+
+
 
 // Show available letters
 
