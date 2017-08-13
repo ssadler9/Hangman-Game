13ab3033bf47f15d41fd0aa51185b0706a04c32f
Sam Sadler <samsadler9@sams-air.attlocal.net>
2017-08-13 17:59:59 -0500

    keyup function now matches with letters in word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,84 +1,59 @@
-
-
 // Words to guess
 var word = ["par", "birdie", "eagle", "bogie", "fairway", "teebox", "rough", "bunker", "green", "flag", "fore"];
-var challenge = "";
 
 // letter bank for user to choose
 var letterBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
-// if (letterBank === "a" || "b" || "c" || "d" || "e" || "f" || "g" || "h" || "i" || "j" || "k" || "l" || "m" || "n" || "o" || "p" || "q" || "r" || "s" || "t" || "u" || "v" || "w" || "x" || "y" || "z") {
-// 	// blank should reveal text
-// 	console.log();
-// } else {
-// 	// incorrect answer, number of choices goes down
-// 	console.log();
-// }
-
 // Randomly chooses words to guess
 var theWord = word[Math.floor(Math.random() * word.length)];
 console.log(theWord);
-
-
 // Counts the length of the string, and displays _ spaces
 var answerSpaces = [];
 for (var i = 0; i < theWord.length; i++) {
-	answerSpaces[i] = "_";
-}
-console.log(answerSpaces);
+    // answerSpaces[i] = "_";
 
+    answerSpaces.push("_");
+}
+// answerSpaces is working
+console.log(answerSpaces);
 // tracks the keys pressed on HTML
 document.onkeyup = function(event) {
     var userGuess = event.key;
-    console.log(userGuess);
+    // commenting out because userGuess works
+    // console.log(userGuess);
 
+    var challengeGuess = [];
 
-// pushes the userGuess into an empty array
-var challengeGuess = [];
-for (var i = 0; i < userGuess.length; i++) {
-    	challengeGuess[i];
-    	challengeGuess.push(userGuess);
-    	console.log(challengeGuess);
-   
- } 
+    challengeGuess.push(userGuess);
+    // challengeGuess is working
+    console.log(challengeGuess);
     // Trying to make the guess match a letter in the generated word
-    	var correctGuess = [];
-    	var incorrectGuess = [];
-    	for (var i = 0; i < theWord.length; i++) {
-    		theWord[i]
-    	if (challengeGuess.indexOf(theWord[i])) {
-	// 	console.log("no");
-	// } else if{
-	// 	console.log("yes");
-	// }
-		challengeGuess.push(incorrectGuess);
-   } else {
-   	challengeGuess.push(correctGuess);
-   		console.log(correctGuess);
-   }
+    var correctGuess = [];
+    var incorrectGuess = [];
+    for (var i = 0; i < theWord.length; i++) {
+
+    	//  This will determine whether the userGuess is a letter in the word
+        if (challengeGuess.indexOf(theWord[i])) {
+
+            // challengeGuess.push(incorrectGuess);
+            incorrectGuess.push(challengeGuess);
+            // this is also showing where the incorrect challengeGuess is in the word
+            console.log("now its time to push this index to an array");
+            // incorrectGuess array is working
+            // console.log(incorrectGuess);
+        } else {
+
+            correctGuess.push(challengeGuess);
+            // this correctly shows which index the letter is in the word
+            console.log("user guess " + userGuess + " found at index " + i);
+            // correctGuess array is working
+            // console.log(correctGuess);
+        }
+    }
+
 }
-}
-
-
-
-
-
 // Blank spaces that coorelate with the hidden letters
-
-
 // Display letters chosen
-
- 
-
 // Display guesses remaining
-
-
-
-
-
 // Show available letters
-
 // Create a box to track wins and losses
-
-
