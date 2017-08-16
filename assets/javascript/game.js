9848e2b9a376d11bf2e89ed1fe0358edaafd799d
Sam Sadler <samsadler9@sams-air.attlocal.net>
2017-08-15 19:15:21 -0500

    javascript now prints on page. letterBank is displayed showing all letters guessed.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,21 @@
 // Words to guess
 var word = ["par", "birdie", "eagle", "bogie", "fairway", "teebox", "rough", "bunker", "green", "flag", "fore"];
 // letter bank for user to choose
-var letterBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Need to write code that displays all letters in alaphbet
-var inhtml =
-    "<p>Letters still available: <br>" + letterBank + "</p>";
-    document.querySelector("#letterBank").innerHTML = inhtml;
-// Need to write code that hides letter after it's been chosen
-
-
+var letterGuessed = [];
+var letterBank = ["a ", "b ", "c ", "d ", "e ", "f ", "g ", "h ", "i ", "j ", "k ", "l ", "m ", "n ", "o ", "p ", "q ", "r ", "s ", "t ", "u ", "v ", "w ", "x ", "y ", "z "];
 // Randomly chooses words to guess
 var theWord = word[Math.floor(Math.random() * word.length)];
 console.log(theWord);
-
 // Counts the length of the string, and displays _ spaces
 var answerSpaces = [];
 for (var i = 0; i < theWord.length; i++) {
     // answerSpaces[i] = "_";
-    answerSpaces.push("_");
+    answerSpaces.push(" _ ");
 }
+printSpaces(answerSpaces);
 // answerSpaces is working
-console.log(answerSpaces);
-
+// console.log(answerSpaces);
+var guessesRemaining = 9;
 // tracks the keys pressed on HTML and matches the key to a letter in the word
 document.onkeyup = function(event) {
     var userGuess = event.key;
@@ -34,61 +28,58 @@ document.onkeyup = function(event) {
     // console.log(challengeGuess);
     // Trying to make the guess match a letter in the generated word
     var correctGuess = [];
-    var incorrectGuess = [""];
-    var guessesRemaining = 9;
-    console.log(guessesRemaining);
+    var incorrectGuess = [];
+    if (theWord.indexOf(challengeGuess) !== -1) {
+
+    } else {
+        // console.log("letter does not exist in word");
+        guessesRemaining--;
+    };
     for (var i = 0; i < theWord.length; i++) {
         //  This will determine whether the userGuess is a letter in the word
         if (challengeGuess.indexOf(theWord[i])) {
             // pushes incorrectGuesses to an array
             incorrectGuess.push(challengeGuess);
-            // this is also showing where the incorrect challengeGuess is in the word
-            // console.log("now its time to push this index to an array");
             // incorrectGuess array is working
             // console.log(incorrectGuess);
         } else {
-        	// pushes the correctGuesses to an array
+            // pushes the correctGuesses to an array
             correctGuess.push(challengeGuess);
             // Display letters chosen in the corresponding blank space  
             answerSpaces.splice([i], 1, userGuess);
-            console.log(answerSpaces);
-
-            // this correctly shows which index the letter is in the word
-            // console.log("user guess " + userGuess + " found at index " + i);
+            // console.log(answerSpaces);
             // correctGuess array is working
             // console.log(correctGuess);
         }
-        // for (var i = 9; i < guessRemaining.length; i--) {
-        // 	if (incorrectGuess !=== theWord[i] ) {
-        // 		guessRemaining[i]
-        // 	} 
-        // }
-        }
-            // sets variable to print javascript to HTML
+    }
+    // Display letters chosen in the corresponding blank space
+	letterGuessed.push(userGuess);
+	// commenting out because letterGuessed is working as expected
+	// console.log(letterGuessed);
+	//Displaying the letters played in the game 
+	var letterguessed =
+        "<p>Letters you've already guessed: " + letterGuessed + "</p>";
+    // querySelector places text into letterGuessed
+    document.querySelector("#letterGuessed").innerHTML = letterguessed;
+    // sets variable to print javascript to HTML
+    printSpaces(answerSpaces);
+    printGuesses(guessesRemaining);
+}
+
+// Prints the amount of spaces avialable in a theWord
+function printSpaces(answerSpacesParameter) {
     var html =
-    "<p>Word to Guess: " + answerSpaces + "</p>";
+        "<p>Word to Guess: " + answerSpacesParameter + "</p>";
     // querySelector places text into answerspace
     document.querySelector("#answerSpaces").innerHTML = html;
 }
+// Shows the amount of Guesses remaining before player loses
+function printGuesses(guessesRemainingParameter) {
+    var guesses =
+        "<p>Guesses Remaining: " + guessesRemaining + "</p>";
+    // querySelector places text into answerspace
+    document.querySelector("#guessesRemaining").innerHTML = guesses;
+}	
 
+printGuesses(guessesRemaining);
 
-
-
-
-
-// if placed outside of function, letters don't fill spaces
-// // sets variable to print javascript to HTML
-//     var html =
-//     "<p>Word to Guess: " + answerSpaces + "</p>";
-//     // querySelector places text into answerspace
-//     document.querySelector("#answerSpaces").innerHTML = html;
-
-
-// Display letters chosen in the corresponding blank space
-
-
-
-
-// Display guesses remaining
-// Show available letters
-// Create a box to track wins and losses
